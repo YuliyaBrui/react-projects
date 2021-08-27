7bd1ccad4731c784526eeb375d3619e8cb3150f1
YuliyaBrui <juljadivina@gmail.com>
2021-08-27 13:03:33 +0300

    feat: add project react_forms

---
+++ src/card/Card.tsx
@@ -0,0 +1,51 @@
+import '../../assets/styles/card.css';
+import React from 'react';
+import { State } from '../form/Form';
+
+interface CardProps {
+  Item: State
+}
+
+export const Card: React.FC<CardProps> = ({ Item }) => (
+  <div className="card-container">
+    <div className="card">
+      <div className="inform">
+        <span className="point">
+          {' '}
+          Name:
+          {' '}
+          <span className="value">{Item.firstName}</span>
+        </span>
+        <br />
+        <span className="point">
+          Surname:
+          <span className="value">{Item.surName}</span>
+        </span>
+        <br />
+        <span className="point">
+          Birh date:
+          {' '}
+          <span className="value">{Item.birthDate}</span>
+        </span>
+        <br />
+        <span className="point">
+          Country:
+          {' '}
+          <span className="value">{Item.country}</span>
+        </span>
+        <br />
+        <span className="point">
+          Email:
+          {' '}
+          <span className="value">{Item.email}</span>
+        </span>
+        <br />
+        <span className="point">
+          To send newsletter:
+          {' '}
+          <span className="value">{Item.toggle ? 'Yes' : 'No'}</span>
+        </span>
+      </div>
+    </div>
+  </div>
+);
---
+++ src/form/Form.tsx
@@ -0,0 +1,286 @@
+import React, { FormEvent, useEffect, useState } from 'react';
+import '../../assets/styles/form.css';
+
+export interface State {
+  firstName: string
+  surName: string
+  birthDate: string
+  country: string
+  email: string
+  agreement: boolean
+  toggle: boolean
+}
+interface Errors {
+  firstName?: string
+  surName?: string
+  birthDate?: string
+  country?: string
+  email?: string
+  agreement?: boolean
+  toggle?: boolean
+}
+interface FormProps {
+  // setFormValues: (arg: (state: State[]) => void) => void
+  setFormValues: React.Dispatch<React.SetStateAction<State[]>>
+}
+
+export const Form: React.FC<FormProps> = ({ setFormValues }) => {
+  const [firstName, setFirstName] = useState('');
+  const [surName, setSurName] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [country, setCountry] = useState('Belarus');
+  const [email, setEmail] = useState('');
+  const [agreement, setAgreement] = useState(false);
+  const [toggle, setToggle] = useState(true);
+  const [errors, setErrors] = useState({} as Errors);
+  const [emailError, setEmailError] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [surNameError, setSurNameError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const validate = () => {
+    setErrors({} as Errors);
+    if (!agreement) {
+      setErrors((state) => ({ ...state, agreement }));
+    }
+    if (firstName === '') {
+      setErrors((state) => ({ ...state, firstName }));
+    }
+    if (surName === '') {
+      setErrors((state) => ({ ...state, surName }));
+    }
+    if (email === '') {
+      setErrors((state) => ({ ...state, email }));
+    }
+
+    if (birthDate === '') {
+      setErrors((state) => ({ ...state, birthDate }));
+    }
+  };
+  useEffect(() => {
+    validate();
+  }, [agreement, firstName, surName, birthDate, email]);
+
+  const emailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(event.target.value).toLowerCase())) {
+      setErrors((state) => ({ ...state, email }));
+      setEmailError('incorrect address');
+    } else {
+      setEmailError('');
+    }
+  };
+  const nameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(event.target.value);
+    const re = /^[а-яёa-z][-а-яёa-z']{1,50}$/i;
+    if (!re.test(String(event.target.value).toLowerCase())) {
+      setErrors((state) => ({ ...state, firstName }));
+
+      setNameError(
+        "Only letters and symbols ' and - . Name should not exceed 50 symbols ",
+      );
+    } else {
+      setNameError('');
+    }
+  };
+  const surNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSurName(event.target.value);
+    const re = /^[а-яёa-z][-а-яёa-z']{1,50}$/i;
+    if (!re.test(String(event.target.value).toLowerCase())) {
+      setErrors((state) => ({ ...state, surName }));
+
+      setSurNameError(
+        "Only letters and symbols ' and - . Surname should not exceed 50 symbols ",
+      );
+    } else {
+      setSurNameError('');
+    }
+  };
+  const reset = () => {
+    setAgreement(false);
+    setFirstName('');
+    setSurName('');
+    setBirthDate('');
+    setCountry('Belarus');
+    setEmail('');
+    setToggle(true);
+  };
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    if (Object.keys(errors).length === 0) {
+      setFormValues((state) => [
+        ...state,
+        {
+          firstName,
+          surName,
+          birthDate,
+          country,
+          email,
+          agreement,
+          toggle,
+        },
+      ]);
+      reset();
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <h2>User Registration</h2>
+      <p className="footnote">
+        {' '}
+        <span className="mark">*</span>
+        - required field
+      </p>
+      <label htmlFor="firstName" className="container-input">
+        <p className="label-input">
+          {' '}
+          <span className="mark">*</span>
+          {' '}
+          Name :
+        </p>
+        <input
+          className={
+            isSubmitting && errors?.firstName === ''
+              ? 'value-empty input_text'
+              : 'input_text'
+          }
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            nameHandler(e);
+          }}
+        />
+        <p className="error">
+          {nameError && <span className="error">{nameError}</span>}
+        </p>
+      </label>
+      <label htmlFor="surName" className="container-input">
+        <p className="label-input">
+          <span className="mark">*</span>
+          Surname:
+        </p>
+        <input
+          className={
+            isSubmitting && errors?.surName === ''
+              ? 'value-empty input_text'
+              : 'input_text'
+          }
+          type="text"
+          name="surName"
+          value={surName}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            surNameHandler(e);
+          }}
+        />
+        <p className="error">
+          {surNameError && <span className="error">{surNameError}</span>}
+        </p>
+      </label>
+      <label htmlFor="birthDate" className="container-input">
+        <p className="label-input">
+          <span className="mark">*</span>
+          Birth date:
+        </p>
+        <input
+          className={
+            isSubmitting && errors?.birthDate === ''
+              ? 'value-empty input_text'
+              : 'input_text'
+          }
+          type="date"
+          name="birthDate"
+          value={birthDate}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const newValue = e.target.value;
+            setBirthDate(newValue);
+          }}
+        />
+      </label>
+      <label htmlFor="country" className="container-select">
+        <p className="label-input">
+          <span className="mark">*</span>
+          Country:
+        </p>
+        <select
+          className="select"
+          name="country"
+          value={country}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+            const newValue = e.target.value;
+            setCountry(newValue);
+          }}
+        >
+          <option>Belarus</option>
+          <option>Kazakhstan</option>
+          <option>Lithuania</option>
+          <option>Poland</option>
+          <option>Russia</option>
+          <option>Ukraine</option>
+        </select>
+      </label>
+      <label htmlFor="email" className="container-input">
+        <p className="label-input">
+          <span className="mark">*</span>
+          E-mail:
+        </p>
+        <input
+          className={
+            isSubmitting && errors?.email === ''
+              ? 'value-empty input_text'
+              : 'input_text'
+          }
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            emailHandler(e);
+          }}
+        />
+        <p className="error">
+          {' '}
+          {emailError && <span className="error">{emailError}</span>}
+        </p>
+      </label>
+      <label htmlFor="toggle" className="toggle">
+        <div className="switch">
+          <input
+            className="switch__checkbox"
+            type="checkbox"
+            name="toggle"
+            checked={toggle}
+            onChange={() => setToggle((prev) => !prev)}
+          />
+          <span className="switch__slider" />
+        </div>
+        <p className="text-agreement">
+          {' '}
+          Do you want to be notified about news and promotions?
+        </p>
+      </label>
+      <label className="checkbox-agree" htmlFor="agreement">
+        <input
+          className="checkbox"
+          type="checkbox"
+          name="agreement"
+          checked={agreement}
+          onChange={() => setAgreement((prev) => !prev)}
+        />
+        <p className="text-agreement">
+          {' '}
+          I agree to the processing of personal data
+          <br />
+          {' '}
+          {errors?.agreement !== undefined && (
+            <span className="error">agreement should be checked</span>
+          )}
+        </p>
+      </label>
+      <div className="button_submit">
+        <input type="submit" value="Send" className="input_submit" />
+      </div>
+    </form>
+  );
+};
